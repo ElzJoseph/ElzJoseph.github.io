947ef80d945a0aff856c6a22978f639220c6681e
ElzJoseph <eljoseph999@gmail.com>
2023-07-08 20:26:20 +0700

    ok

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.menu-toggle').addEventListener('click', function() {
         document.querySelector('.menu-nav').classList.toggle('clicked');
     })
-    document.querySelector('.icon').addEventListener('click', function() {
-        document.querySelector('.icon').classList.toggle('clicked');
-    })
+    // document.querySelector('.icon').addEventListener('click', function() {
+    //     document.querySelector('.icon').classList.toggle('clicked');
+    // })
 })
